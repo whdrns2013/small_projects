75fd7d736751341c07d79b1df152d3e03fc99b0a
Jonghyuk Park <whdrns2013@naver.com>
2025-01-08 12:27:38 +0900

    update project

---
+++ dq_small_game/dq_small_games/android/app/src/main/kotlin/com/example/dq_small_games/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dq_small_games
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dq_small_game/dq_small_games/lib/screens/home_screen.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key})
+}
