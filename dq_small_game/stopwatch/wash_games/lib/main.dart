a1f1192cce4da1df9f1b2a52e94c85b21ff51ca9
whdrns2013@naver.com <62002568+whdrns2013@users.noreply.github.com>
2025-01-26 00:52:19 +0900

    memo references

--- dq_small_game/stopwatch/wash_games/lib/main.dart
+++ dq_small_game/stopwatch/wash_games/lib/main.dart
@@ -19,3 +19,13 @@ class MyApp extends StatelessWidget {
         ));
   }
 }
+
+
+/*
+references
+위젯 겹치기 : https://ahang.tistory.com/24  
+다트 논리 연산자 : https://needneo.tistory.com/229  
+화면 스크롤 기능 : https://dudgus907.tistory.com/7  
+주석 : https://while1.tistory.com/94#google_vignette  
+기타 : https://chatgpt.com/share/67950838-7698-800f-9e28-bbde296d2a4e  
+*/
